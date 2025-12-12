e6e2f0a2805dc1ca1a25668b407de04ec30e9235
Darachhat <darachhat012@gmail.com>
2025-12-12 09:57:23 +0700

    implement cart deatils

---
+++ app/src/main/java/kh/sothun/darachhat/rupp/fe/ecommerce_app/activities/CartActivity.kt
@@ -0,0 +1,21 @@
+package kh.sothun.darachhat.rupp.fe.ecommerce_app.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kh.sothun.darachhat.rupp.fe.ecommerce_app.R
+
+class CartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_cart)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
